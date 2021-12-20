b3ffd4fbc82c9cf6ce6b824222e3c52fb46af4a6
jmon83 <76069447+jmon83@users.noreply.github.com>
2021-12-20 13:12:00 -0500

    Another

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
+
 import './App.css';
 
 function App() {
   return (
-      <div className="flex items-center w-full px-4 py-10 bg-cover card bg-base-200"
-           style="background-image: url(&quot;https://picsum.photos/id/314/1000/300&quot;);">
-        <div className="card glass lg:card-side text-neutral-content">
-          <figure className="p-6">
-           //COUNTDOWN
-          </figure>
-          <div className="max-w-md card-body">
-            <h2 className="card-title">Stop Watch</h2>
-            <p>A simple React stop watch for the Altcademy. Made with Tailwind and DasiyUI. </p>
-            <div className="card-actions">
-              //Buttons
+    <div className="App">
+        <div className="flex items-center w-full px-4 py-10 bg-cover card bg-base-200">
+            <div className="card glass lg:card-side text-neutral-content">
+                <figure className="p-6">
+                    //COUNTDOWN
+                </figure>
+                <div className="max-w-md card-body">
+                    <h2 className="card-title">Stop Watch</h2>
+                    <p>A simple React stop watch for the Altcademy. Made with Tailwind and DasiyUI. </p>
+                    <div className="card-actions">
+                        //Buttons
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
+    </div>
   );
 }
 
